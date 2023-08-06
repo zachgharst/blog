e706e2f37f9eb13a37a208fb5d48fd9bdfc834b1
Zach Gharst <zdgharst@gmail.com>
2023-08-05 19:18:51 -0500

    Start the web server project

---
+++ cmd/blogger/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+)
+
+// todo:
+//   have link to all blogs (in some semblance) on this page
+//   make sure route is / in URL when page loaded
+func getAllBlogs(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("GET getAllBlogs\n")
+
+    homepageTemplate, err := template.ParseFiles("static/index.html")
+
+    if err != nil {
+        panic("Problem reading homepage template")
+    }
+
+    if homepageTemplate.Execute(w, "") != nil {
+        fmt.Fprintf(w, "Problem using template file")
+    }
+}
+
+// todo:
+//   proper 404 if blog is not found
+//   for SEO: should be /blog/[some string identifying the title of the blog]
+//   don't allow FS access via route, names should match to a file via GUID
+func getBlog(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/blog/")
+    fileName := fmt.Sprintf("blogs/%s.md", id)
+    fmt.Printf("GET getBlog: [%s] with filename [%s]\n", id, fileName)
+
+    data, err := os.ReadFile(fileName)
+    if err != nil {
+        fmt.Fprint(w, "404")
+        fmt.Printf("Problem reading file %s\n", fileName)
+        return
+    }
+
+    html := string(mdToHTML(data))
+    blogTemplate, err := template.ParseFiles("static/blog.html")
+
+    if err != nil {
+        panic("Problem reading blog template")
+    }
+
+    if blogTemplate.Execute(w, template.HTML(html)) != nil {
+        fmt.Fprintf(w, "Problem using template file")
+    }
+}
+
+func main() {
+	http.HandleFunc("/", getAllBlogs)
+	http.HandleFunc("/blog/", getBlog)
+
+	fmt.Println("Starting web server on port 8080...")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+func mdToHTML(md []byte) []byte {
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse(md)
+
+	htmlFlags := html.CommonFlags
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
+}
